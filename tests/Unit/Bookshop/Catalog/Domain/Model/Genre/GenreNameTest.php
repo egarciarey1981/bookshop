417c1bba2fc8cc0f4fa0bbb2add08c48c36637d7
Eliecer Garcia Rey <erey@freepik.com>
2023-11-27 21:51:28 +0100

    Update dependencies and fix GenreName validation

--- tests/Unit/Bookshop/Catalog/Domain/Model/Genre/GenreNameTest.php
+++ tests/Unit/Bookshop/Catalog/Domain/Model/Genre/GenreNameTest.php
@@ -2,12 +2,38 @@
 
 namespace Test\Unit\Bookshop\Catalog\Domain\Model\Genre;
 
+use Bookshop\Catalog\Domain\Exception\DomainException;
+use Bookshop\Catalog\Domain\Model\Genre\GenreName;
 use PHPUnit\Framework\TestCase;
 
 class GenreNameTest extends TestCase
 {
+    private const MIN_LENGTH = 3;
+    private const MAX_LENGTH = 255;
+
+    public function testCreate(): void
+    {
+        $genreName = new GenreName('Genre name');
+        $this->assertEquals('Genre name', $genreName->value());
+    }
+
+    public function testCheckValueEmpty(): void
+    {
+        $this->expectException(DomainException::class);
+        new GenreName('');
+    }
+
     public function testCheckValueTooShort(): void
     {
-        $this->assertTrue(true);
+        $this->expectException(DomainException::class);
+        $value =  str_repeat('a', self::MIN_LENGTH - 1);
+        new GenreName($value);
+    }
+
+    public function testCheckValueTooLong(): void
+    {
+        $this->expectException(DomainException::class);
+        $value = str_repeat('a', self::MAX_LENGTH + 1);
+        new GenreName($value);
     }
 }
