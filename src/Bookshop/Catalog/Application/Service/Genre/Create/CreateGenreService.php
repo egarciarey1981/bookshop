5a08fd19c4d7bdd3ba4e686ec089fed8ab70ba85
Eliecer Garcia Rey <erey@freepik.com>
2023-12-15 18:47:08 +0100

    fic dont use fromPrimitives

--- src/Bookshop/Catalog/Application/Service/Genre/Create/CreateGenreService.php
+++ src/Bookshop/Catalog/Application/Service/Genre/Create/CreateGenreService.php
@@ -3,6 +3,8 @@
 namespace Bookshop\Catalog\Application\Service\Genre\Create;
 
 use Bookshop\Catalog\Domain\Model\Genre\Genre;
+use Bookshop\Catalog\Domain\Model\Genre\GenreName;
+use Bookshop\Catalog\Domain\Model\Genre\GenreNumberOfBooks;
 use Bookshop\Catalog\Domain\Model\Genre\GenreRepository;
 
 class CreateGenreService
@@ -17,11 +19,13 @@ class CreateGenreService
     public function execute(CreateGenreRequest $request): CreateGenreResponse
     {
         $genreId = $this->genreRepository->nextIdentity();
+        $genreName = new GenreName($request->name());
+        $genreNumberOfBooks = new GenreNumberOfBooks(0);
 
-        $genre = Genre::fromPrimitives(
-            $genreId->value(),
-            $request->name(),
-            0,
+        $genre = new Genre(
+            $genreId,
+            $genreName,
+            $genreNumberOfBooks,
         );
 
         $this->genreRepository->insert($genre);
